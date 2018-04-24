b48e0024b1f7ba46033fe79581ece93d73cfd4c8
Richard Otte <rotte@blackducksoftware.com>
2018-04-24 15:11:09 -0400

    adding buildscript, updating version, updating license header

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A simple shell script that builds the Black Duck Eclipse plugin solution.
+# Passes maven args through to both reactor builds.
+
+# Due to Tycho limitations, pom-first Maven dependencies must be resolved in a
+# separate reactor first...
+echo "Wrapping solution dependencies into OSGI bundle..."
+cd ./bundles/blackduck-eclipse-plugin-maven-dependencies/
+mvn clean install $@
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo "Could not create dependencies OSGI bundle."; exit $rc
+fi
+echo "Solution dependency OSGI bundle created."
+
+# ...then we can build the rest of the project.
+echo "Building solution..."
+cd ../../
+mvn clean package $@
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo "Solution was not built successfully."; exit $rc
+fi
+echo "Solution built successfully."
